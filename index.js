5e4985db9272b48d45a374ec95508e450d6adae6
austin samsel <austinsamsel@gmail.com>
2016-02-16 20:50:08 -0500

    add content, create design of first few sections

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ var cssvariables = require('postcss-css-variables')
 var compressor = require('node-minify')
 var conditionals = require('postcss-conditionals')
 var customMedia = require("postcss-custom-media")
+var simpleVars = require("postcss-simple-vars")
+var purify = require('purify-css');
+var cssnext = require('postcss-cssnext');
+
 
 // css to be processed
 var css = fs.readFileSync("src/mnml.css", "utf8")
@@ -16,8 +20,10 @@ var css = fs.readFileSync("src/mnml.css", "utf8")
 var output = postcss([autoprefixer])
   .use(atImport())
   .use(cssvariables())
+  .use(simpleVars())
   .use(conditionals())
   .use(customMedia())
+  .use(cssnext())
   .process(css, {
     from: "./src/mnml.css",
     to: "./css/mnml.css"
@@ -26,9 +32,23 @@ var output = postcss([autoprefixer])
 
 fs.writeFile("css/mnml.css", output, 'utf-8')
 
+
 // Using Sqwish for CSS
 new compressor.minify({
     type: 'sqwish',
     fileIn: './css/mnml.css',
     fileOut: './css/mnml.min.css'
 });
+
+var content = ['index.html'];
+var css = ['css/mnml.min.css'];
+
+var options = {
+  minify: true,
+  output: 'css/mnml.min.css'
+};
+
+// purify(content, css, options, function (purifiedAndMinifiedResult) {
+//   console.log(purifiedAndMinifiedResult);
+// });
+// console.log('updog')
